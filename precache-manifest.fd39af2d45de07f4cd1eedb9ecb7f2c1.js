23bb95ea2629d8743ad12e3b5a75dcafa43e695b
Ajay Jha <ajkmrjha@gmail.com>
2019-01-24 17:00:18 +0530

    Updates

---
+++ precache-manifest.fd39af2d45de07f4cd1eedb9ecb7f2c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "078c3ee6e4776fc29ab3",
+    "url": "/website/static/css/main.72319754.chunk.css"
+  },
+  {
+    "revision": "078c3ee6e4776fc29ab3",
+    "url": "/website/static/js/main.078c3ee6.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06eb3ed044fa38689a19cb9d152b37dc",
+    "url": "/website/index.html"
+  }
+];
