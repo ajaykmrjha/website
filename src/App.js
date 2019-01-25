dc380d77173c7b8093487db6a26bccb74d055042
Ajay Jha <ajkmrjha@gmail.com>
2019-01-25 10:37:30 +0530

    Changed H1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <h1>Hello World React App</h1>
       </div>
     );
   }
